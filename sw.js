c9706248e42048cfd0cbbb99dacac3be6ba067ef
Senapelan <arsip.senapelan@gmail.com>
2026-02-08 03:12:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,77 +1,23 @@
-const CACHE_NAME = 'senapelan-v1.0';
+const CACHE_NAME = 'senapelan-cache-v1';
 const urlsToCache = [
-  '/Home/',
-  '/Home/index.html',
-  '/Home/data/foto/icon512.png',
-  '/Home/data/foto/icon192.png',
-  '/Home/data/video/home.mp4',
-  '/Home/data/sound/home.mp3'
+  '/',
+  '/index.html',
+  '/data/foto/icon512.png',
+  '/data/foto/icon192.png'
 ];
 
-// Install Service Worker
+// Install
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache opened');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch Strategy: Cache First, then Network
+// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Return cached version
-        if (response) {
-          return response;
-        }
-        
-        // Clone the request
-        const fetchRequest = event.request.clone();
-        
-        return fetch(fetchRequest)
-          .then(response => {
-            // Check if valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            
-            // Clone the response
-            const responseToCache = response.clone();
-            
-            // Cache the new response
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-              
-            return response;
-          })
-          .catch(() => {
-            // If offline and request is for HTML, return offline page
-            if (event.request.headers.get('accept').includes('text/html')) {
-              return caches.match('/Home/');
-            }
-          });
-      })
-  );
-});
-
-// Activate and Clean Old Caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
